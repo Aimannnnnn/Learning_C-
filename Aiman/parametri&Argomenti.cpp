c6e2066286b879f2952cbf836af7d60b461368c1
Aiman <aimanmohammad58@gmail.com>
2024-06-03 21:15:59 +0200

    Added Example arguments/parameters

---
+++ Aiman/parametri&Argomenti.cpp
@@ -0,0 +1,13 @@
+  #include <iostream>
+  using namespace std;
+  
+  void funzione (string parametro1, string parametro2) {
+    cout << parametro1 << parametro2;
+}
+
+  int main () {
+    string str1 = " \n Argomento2";
+    funzione("Argomento1", str1);
+  return 0;
+}
+
